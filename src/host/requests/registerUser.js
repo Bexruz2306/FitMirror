c5384baa78a849ef640cf9d6f16e406ad802ee8a
Bexruz <baxranovbexruzdevloper@gmail.com>
2026-05-07 14:38:46 +0500

    fixed

--- src/host/requests/registerUser.js
+++
@@ -1,8 +0,0 @@
-import { httpPostRequisets } from "../Requiests"
-
-export const registerUser=(data)=>{
-    return (httpPostRequisets('/users/register',data))
-}
-export const loginUser=(data)=>{
-    return (httpPostRequisets('/users/login',data))
-}
